4a632260ca13c8ee4ef54ef9dad014d6ae48c762
Abdulbari Qaisar <abdulbari.112917@gmail.com>
2022-10-14 23:57:54 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 // Write your code here
+const num1 = 31;
+const num2=2;
+
+const multiply = num1*num2;
+
+
+const num3 = 4;
+const num4=31;
+const mod = num3 % num4;
+
+const max = Math.max(12 , 20 ,11 , 2 ,3 ,4 ,17);
+const random = (Math.floor( 10*Math.random() ));
